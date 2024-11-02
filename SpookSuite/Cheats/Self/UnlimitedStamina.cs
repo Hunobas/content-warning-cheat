e7a4d42cbef11e7c6abad1875ec8e33cb8ba4ab5
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:37:01 +0900

    update file

---
+++ SpookSuite/Cheats/Self/UnlimitedStamina.cs
@@ -0,0 +1,15 @@
+﻿using SpookSuite.Cheats.Core;
+
+namespace SpookSuite.Cheats
+{
+    internal class UnlimitedStamina : ToggleCheat
+    {
+        public override void Update()
+        {
+            if (Player.localPlayer is null || !Enabled || Player.localPlayer.refs.controller is null) return;
+
+            Player.localPlayer.data.currentStamina = Player.localPlayer.refs.controller.maxStamina;
+        }
+
+    }
+}
