406c7cf31544679bbd28c05c19840d16d98e8697
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:36:55 +0900

    update file

---
+++ SpookSuite/Cheats/Self/NameSpoof.cs
@@ -0,0 +1,32 @@
+﻿using Photon.Pun;
+using Steamworks;
+using SpookSuite.Cheats.Core;
+
+namespace SpookSuite.Cheats
+{
+    internal class NameSpoof : ToggleCheat, IVariableCheat<string>
+    {
+        public static string Value = SteamFriends.GetPersonaName();
+
+        public static void TrySetNickname()
+        {
+            if (!Instance<NameSpoof>().Enabled) return;
+
+            PhotonNetwork.LocalPlayer.NickName = Value;
+        }
+
+        public override void OnEnable()
+        {
+            PhotonNetwork.LocalPlayer.NickName = Value;
+        }
+        public override void OnDisable()
+        {
+            PhotonNetwork.LocalPlayer.NickName = SteamFriends.GetPersonaName();
+        }
+
+        public static void OnValueChanged(string newValue)
+        {
+            PhotonNetwork.LocalPlayer.NickName = newValue;
+        }
+    }
+}
