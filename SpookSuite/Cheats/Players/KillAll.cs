801cbb52f8251c048e0b83773e7e0c9de9d7dad5
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:36:49 +0900

    update file

---
+++ SpookSuite/Cheats/Players/KillAll.cs
@@ -0,0 +1,16 @@
+﻿using SpookSuite.Cheats.Core;
+using SpookSuite.Manager;
+using SpookSuite.Util;
+using System.Linq;
+
+namespace SpookSuite.Cheats
+{
+    internal class KillAll : ExecutableCheat
+    {
+        public override void Execute()
+        {
+            foreach (var p in GameObjectManager.players.Where(p => !p.IsLocal))
+                if (!p.data.dead) p.Reflect().Invoke("CallDie");
+        }
+    }
+}
