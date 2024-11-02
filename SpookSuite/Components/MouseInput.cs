6696db887ebcbe3d0e05bc6096f2f7e6da07e8a3
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:37:06 +0900

    update file

---
+++ SpookSuite/Components/MouseInput.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace SpookSuite.Components
+{
+    internal class MouseInput : MonoBehaviour
+    {
+        private float Yaw = 0f;
+        private float Pitch = 0f;
+
+        private void Update()
+        {
+            if(Cursor.visible) return;
+
+            Yaw += Input.GetAxis("Mouse X");
+            Yaw = (Yaw + 360f) % 360f;
+
+            Pitch -= Input.GetAxis("Mouse Y");
+            Pitch = Mathf.Clamp(Pitch, -90f, 90f);
+
+            transform.eulerAngles = new Vector3(Pitch, Yaw, 0f);
+        }
+    }
+}
