667d1ebf99d3b36ff259d87d14b2b431d6398f82
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:37:04 +0900

    update file

---
+++ SpookSuite/Cheats/Visual/FOV.cs
@@ -0,0 +1,22 @@
+﻿using SpookSuite.Cheats.Core;
+using SpookSuite.Util;
+
+namespace SpookSuite.Cheats
+{
+    internal class FOV : ToggleCheat, IVariableCheat<float>
+    {
+        public static float Value = 70f;
+
+        public override void Update()
+        {
+            if (Player.localPlayer is null || !Enabled)
+                return;
+
+            MainCamera.instance.Reflect().SetValue("baseFOV", Value);
+        }
+        public override void OnDisable()
+        {
+            MainCamera.instance.Reflect().SetValue("baseFOV", 70f);
+        }
+    }
+}
