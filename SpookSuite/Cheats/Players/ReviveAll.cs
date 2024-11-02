0577cc3e2658d52fbbeb804620416b615420c0df
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:36:52 +0900

    update file

---
+++ SpookSuite/Cheats/Players/ReviveAll.cs
@@ -0,0 +1,17 @@
+﻿using SpookSuite.Cheats.Core;
+using SpookSuite.Manager;
+
+namespace SpookSuite.Cheats
+{
+    internal class ReviveAll : ExecutableCheat
+    {
+        public override void Execute()
+        {
+            foreach (Player p in GameObjectManager.players)
+            {
+                if (p.data.dead) p.CallRevive();
+            }
+        }
+
+    }
+}
