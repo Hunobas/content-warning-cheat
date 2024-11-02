9da084051033626a01ada0d35fb9b2cc6088a799
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:36:59 +0900

    update file

---
+++ SpookSuite/Cheats/Self/SuperSpeed.cs
@@ -0,0 +1,21 @@
+﻿using SpookSuite.Cheats.Core;
+
+namespace SpookSuite.Cheats
+{
+    internal class SuperSpeed : ToggleCheat, IVariableCheat<float>
+    {
+        public static float Value = 60f;
+
+        public override void Update()
+        {
+            if (Player.localPlayer is null || !Enabled) return;
+
+            Player.localPlayer.gameObject.GetComponent<PlayerController>().movementForce = Value;
+        }
+
+        public override void OnDisable()
+        {
+            Player.localPlayer.gameObject.GetComponent<PlayerController>().movementForce = 17f;
+        }
+    }
+}
