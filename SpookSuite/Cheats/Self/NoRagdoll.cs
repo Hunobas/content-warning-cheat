96805ff35f8d81c19092b705dbae37607f1e21f3
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:36:56 +0900

    update file

---
+++ SpookSuite/Cheats/Self/NoRagdoll.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using SpookSuite.Cheats.Core;
+
+namespace SpookSuite.Cheats
+{
+    [HarmonyPatch]
+    internal class NoRagdoll : ToggleCheat
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Player), "Ragdoll")]
+        public static bool Ragdoll(Player __instance, ref bool __result)
+        {
+            if (Instance<NoRagdoll>().Enabled && !__instance.data.dead)
+            {
+                __result = false;
+                return false;
+            }              
+            return true;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(Player), "NoControl")]
+        public static bool NoControl(Player __instance, ref bool __result)
+        {
+            if (Settings.b_isMenuOpen)
+            {
+                __result = true;
+                return false;
+            }
+
+            if (Instance<NoRagdoll>().Enabled && !__instance.data.dead)
+            {
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+    }
+}
