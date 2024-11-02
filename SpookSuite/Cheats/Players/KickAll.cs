b303d4fe7f0e337e4d1a25f03573b585b17284d7
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:36:49 +0900

    update file

---
+++ SpookSuite/Cheats/Players/KickAll.cs
@@ -0,0 +1,16 @@
+﻿using Photon.Pun;
+using SpookSuite.Cheats.Core;
+using SpookSuite.Handler;
+using SpookSuite.Manager;
+
+namespace SpookSuite.Cheats
+{
+    internal class KickAll : ExecutableCheat
+    {
+        public override void Execute()
+        {
+            SurfaceNetworkHandler.Instance.photonView.RPC("RPC_LoadScene", PhotonNetwork.LocalPlayer.IsMasterClient ?
+                GameObjectManager.players.Find(p => p != Player.localPlayer).PhotonPlayer() : PhotonNetwork.MasterClient, "NewMainMenu");
+        }
+    }
+}
