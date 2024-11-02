419083eca1bd8d57a1944c92e5c9a036d9dbc8f4
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:36:58 +0900

    update file

---
+++ SpookSuite/Cheats/Self/Spinbot.cs
@@ -0,0 +1,9 @@
+﻿using SpookSuite.Cheats.Core;
+
+namespace SpookSuite.Cheats
+{
+    public class Spinbot : ToggleCheat, IVariableCheat<float>
+    {
+        public static float Value = 1f;
+    }
+}
