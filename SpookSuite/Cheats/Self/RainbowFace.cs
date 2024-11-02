606901191cb466e7b9d2c0186db156c313655176
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:36:56 +0900

    update file

---
+++ SpookSuite/Cheats/Self/RainbowFace.cs
@@ -0,0 +1,22 @@
+﻿using SpookSuite.Cheats.Core;
+using SpookSuite.Util;
+
+namespace SpookSuite.Cheats
+{
+    internal class RainbowFace : ToggleCheat, IVariableCheat<float>
+    {
+        RainbowController rgb = new RainbowController();
+
+        public static float Value = 0.1f; //interval
+
+        public override void Update()
+        {
+            if (!Enabled || Player.localPlayer is null) return;
+
+            rgb.speed = Value;
+            rgb.Update();
+
+            Player.localPlayer.refs.visor.ApplyVisorColor(rgb.GetColor());
+        }
+    }
+}
