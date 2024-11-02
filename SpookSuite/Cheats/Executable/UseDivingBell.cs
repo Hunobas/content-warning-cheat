a685672466e22a70ca7d1ba62d22d5bbdae8d64f
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:36:43 +0900

    update file

---
+++ SpookSuite/Cheats/Executable/UseDivingBell.cs
@@ -0,0 +1,24 @@
+﻿using SpookSuite.Cheats.Core;
+using SpookSuite.Manager;
+using System.Collections;
+using UnityEngine;
+
+namespace SpookSuite.Cheats
+{
+    internal class UseDivingBell : ExecutableCheat
+    {
+        public override void Execute() => SpookSuite.Instance.StartCoroutine(UseCoroutine());
+
+        public IEnumerator UseCoroutine()
+        {
+            if (GameObjectManager.divingBell.opened)
+                Cheat.Instance<ToggleDivingBell>().Execute();
+
+            yield return new WaitForSeconds(2.6f);
+
+            if (GameObjectManager.divingBell.onSurface)
+                GameObjectManager.divingBell.GoUnderground();
+            else GameObjectManager.divingBell.GoToSurface();
+        }
+    }
+}
