262ca91f7459db07902618413fea069302cf560c
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:37:01 +0900

    update file

---
+++ SpookSuite/Cheats/Self/UnlimitedOxygen.cs
@@ -0,0 +1,14 @@
+﻿using SpookSuite.Cheats.Core;
+
+namespace SpookSuite.Cheats
+{
+    internal class UnlimitedOxygen : ToggleCheat
+    {
+        public override void Update()
+        {
+            if (Player.localPlayer is null || !Enabled) return;
+
+            Player.localPlayer.data.remainingOxygen = Player.localPlayer.data.maxOxygen;
+        }
+    }
+}
