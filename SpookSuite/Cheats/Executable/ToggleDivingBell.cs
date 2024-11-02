748ece9457f59394d093636a9136db33f2c3b555
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:36:41 +0900

    update file

---
+++ SpookSuite/Cheats/Executable/ToggleDivingBell.cs
@@ -0,0 +1,16 @@
+﻿using SpookSuite.Cheats.Core;
+
+namespace SpookSuite.Cheats
+{
+    internal class ToggleDivingBell : ExecutableCheat
+    {
+        public override void Execute()
+        {
+            DivingBell bell = FindObjectOfType<DivingBell>();
+
+            if (bell is null) return;
+
+            bell.AttemptSetOpen(!bell.opened);
+        }
+    }
+}
