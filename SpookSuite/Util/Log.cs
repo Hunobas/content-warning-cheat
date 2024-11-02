668f6a07333490119fad2eb9fb5f777b1a77e8cb
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:37:27 +0900

    update file

---
+++ SpookSuite/Util/Log.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace SpookSuite.Util
+{
+    public class Log
+    {
+        private static readonly string prefix = "[SpookSuite]";
+        public static void Info(object message) => Debug.Log($"{prefix} {message}");
+        public static void Warning(object message) => Debug.LogWarning($"{prefix} {message}");
+        public static void Error(object message) => Debug.LogError($"{prefix} {message}");
+
+    }
+}
