6d0c94cc0d02c990129a447db84938745745ee49
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:37:00 +0900

    update file

---
+++ SpookSuite/Cheats/Self/UnlimitedFilm.cs
@@ -0,0 +1,21 @@
+﻿using SpookSuite.Cheats.Core;
+using SpookSuite.Util;
+
+namespace SpookSuite.Cheats
+{
+    internal class UnlimitedFilm : ToggleCheat
+    {
+        public override void Update()
+        {
+            if (Player.localPlayer is null || !Enabled) return;
+
+            ItemInstance item = Player.localPlayer.data.currentItem;
+            if (item is null || item.GetComponent<VideoCamera>() is null) return;
+            VideoCamera camera = item.GetComponent<VideoCamera>();
+
+            VideoInfoEntry videoInfoEntry = camera.Reflect().GetValue<VideoInfoEntry>("m_recorderInfoEntry");
+            videoInfoEntry.timeLeft = videoInfoEntry.maxTime;  
+        }
+
+    }
+}
