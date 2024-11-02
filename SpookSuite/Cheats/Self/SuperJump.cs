5cf7f9f663fecd861e79a74597ff395799df69ff
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:36:59 +0900

    update file

---
+++ SpookSuite/Cheats/Self/SuperJump.cs
@@ -0,0 +1,21 @@
+﻿using SpookSuite.Cheats.Core;
+
+namespace SpookSuite.Cheats
+{
+    internal class SuperJump : ToggleCheat, IVariableCheat<float>
+    {
+        public static float Value = 10f;
+
+        public override void Update()
+        {
+            if (Player.localPlayer is null || !Enabled) return;
+           
+            Player.localPlayer.gameObject.GetComponent<PlayerController>().jumpForceOverTime = Value;
+        }
+
+        public override void OnDisable()
+        {
+            Player.localPlayer.refs.controller.jumpForceOverTime = .6f;
+        }
+    }
+}
