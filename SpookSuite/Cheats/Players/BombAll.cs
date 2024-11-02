85c6f52b0921f87930f7f1537994d00693541e51
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:36:48 +0900

    update file

---
+++ SpookSuite/Cheats/Players/BombAll.cs
@@ -0,0 +1,17 @@
+﻿using SpookSuite.Cheats.Core;
+using SpookSuite.Manager;
+using SpookSuite.Util;
+using System.Linq;
+
+namespace SpookSuite.Cheats
+{
+    internal class BombAll : ExecutableCheat
+    {
+        public static int Value = 1;
+        public override void Execute()
+        {
+            foreach (var p in GameObjectManager.players.Where(p => !p.IsLocal))
+                if (!p.data.dead) GameUtil.SpawnItem(GameUtil.GetItemByName("Bomb").id, p.refs.cameraPos.position, false, false, Value);
+        }
+    }
+}
