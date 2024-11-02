aa7807048ca68303046569b433ceda7cab187fb4
박태훈 <90160859+Hunobas@users.noreply.github.com>
2024-11-02 23:37:12 +0900

    update file

---
+++ SpookSuite/Menu/Core/MenuTab.cs
@@ -0,0 +1,16 @@
+﻿namespace SpookSuite.Menu.Core
+{
+    internal class MenuTab : MenuFragment
+    {
+        public string name;
+        public bool isDebug;
+
+        public MenuTab(string name, bool isDebug = false)
+        {
+            this.name = name;
+            this.isDebug = isDebug;
+        }
+
+        public virtual void Draw() { }
+    }
+}
